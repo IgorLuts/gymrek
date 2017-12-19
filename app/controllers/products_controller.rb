908991ddf7ea348a46931a7166f15cdb0596d5cd
Igor Luts <i.luts.igor@gmail.com>
2017-12-19 23:07:08 +0200

    Корректировка сортировки товаров на главной - 2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
 
   def index
     @q = Product.ransack(params[:q])
-    @products = @q.result.page(params[:page])
+    @products = @q.result.joins(:categories).order('categories.name ASC').page(params[:page])
   end
 
   def show
