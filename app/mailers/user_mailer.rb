db69e1b0a23f425d49f99b336a8bb08ea073db92
Igor Luts <i.luts.igor@gmail.com>
2018-02-06 23:20:39 +0200

    Корректировка имейла

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,16 @@
 class UserMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
-    mail(to: ['i.luts.aot@gmail.com'], subject: 'Website Contact')
+    mail(to: ['i.luts.aot@gmail.com, juliard.club@gmail.com'], subject: 'Website Contact')
   end
 
   def order_notification(order)
     @order = order
-    mail(to: ['i.luts.aot@gmail.com'], subject: 'Order received')
+    mail(to: ['i.luts.aot@gmail.com, juliard.club@gmail.com'], subject: 'Order received')
   end
 
   def callback(contact)
     @contact = contact
-    mail(to: ['i.luts.aot@gmail.com'], subject: 'Callback')
+    mail(to: ['i.luts.aot@gmail.com, juliard.club@gmail.com'], subject: 'Callback')
   end
 end
